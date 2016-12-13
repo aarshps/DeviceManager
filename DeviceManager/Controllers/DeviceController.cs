0ad8f5058431dc38c8ce3975e3814f924205742b
Adarsh PS <adarshps@GMG.LOCAL>
2016-12-13 20:16:16 +0530

    Initial commit

---
+++ DeviceManager/Controllers/DeviceController.cs
@@ -0,0 +1,21 @@
+﻿using DeviceManager.Filters;
+using DeviceManager.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace DeviceManager.Controllers
+{
+    [AllowCors]
+    [RoutePrefix("device")]
+    public class DeviceController : ApiController
+    {
+        [HttpGet]
+        [Route("get-all-devices")]
+        public List<Device> GetAllDevices()
+        {
+            DEV_DMDBEntities context = new DEV_DMDBEntities();
+            return context.Devices.ToList();
+        }
+    }
+}
---
+++ DeviceManager/Filters/AllowCorsAttribute.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Cors;
+using System.Web.Http.Cors;
+
+namespace DeviceManager.Filters
+{
+    public class AllowCorsAttribute : Attribute, ICorsPolicyProvider
+    {
+        private CorsPolicy policy ;
+
+        public Task<CorsPolicy> GetCorsPolicyAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (policy == null)
+            {
+                var retval = new CorsPolicy();
+                retval.AllowAnyHeader = true;
+                retval.AllowAnyMethod = true;
+                retval.AllowAnyOrigin = true;
+                retval.SupportsCredentials = true;
+                policy = retval;
+            }
+            return Task.FromResult(policy);
+        }
+    }
+
+}
